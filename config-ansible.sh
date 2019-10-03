dd26233234bb2913873a3e5e2cbcf439d817a6b6
Hazhir Bakhishi <hazhir.baxishi@gmail.com>
2019-10-03 22:32:17 +0330

    add congig ansible ant its playbooks to the project the play book only configure docker host so far

---
+++ config-ansible.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+USER=vagrant
+PASSWORD=vagrant
+
+# add addresses to /etc/hosts
+#sed -i '/192.168.56/d' /etc/hosts
+grep -v 192.168.56 /etc/hosts > /tmp/h && cat /tmp/h > /etc/hosts
+echo "192.168.56.101 web.com" | sudo tee -a /etc/hosts
+echo "192.168.56.102 db.com" | sudo tee -a /etc/hosts
+echo "192.168.56.103 docker.com" | sudo tee -a /etc/hosts
+#echo " " | sudo tee -a /etc/ansible/hosts
+echo "" > /etc/ansible/hosts
+echo "[all]" | sudo tee -a /etc/ansible/hosts
+echo "web.com" | sudo tee -a /etc/ansible/hosts
+echo "db.com" | sudo tee -a /etc/ansible/hosts
+echo "docker.com 	ansible_ssh_private_key_file=/vagrant/.vagrant/machines/docker/virtualbox/private_key" | sudo tee -a /etc/ansible/hosts
+ansible-playbook /home/vagrant/playbooks/install-docker.yaml
