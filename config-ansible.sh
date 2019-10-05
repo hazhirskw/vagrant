b9f94d9ed1396add833591c8357666e5c7588247
Hazhir Bakhishi <hazhir.baxishi@gmail.com>
2019-10-05 11:10:18 +0330

    add private key file locations for web nad db hosts to the ansible hosts file

--- config-ansible.sh
+++ config-ansible.sh
@@ -12,7 +12,7 @@ echo "192.168.56.103 docker.com" | sudo tee -a /etc/hosts
 #echo " " | sudo tee -a /etc/ansible/hosts
 echo "" > /etc/ansible/hosts
 echo "[all]" | sudo tee -a /etc/ansible/hosts
-echo "web.com" | sudo tee -a /etc/ansible/hosts
-echo "db.com" | sudo tee -a /etc/ansible/hosts
+echo "web.com 	ansible_ssh_private_key_file=/vagrant/.vagrant/machines/web/virtualbox/private_key"| sudo tee -a /etc/ansible/hosts
+echo "db.com 	ansible_ssh_private_key_file=/vagrant/.vagrant/machines/db/virtualbox/private_key" | sudo tee -a /etc/ansible/hosts
 echo "docker.com 	ansible_ssh_private_key_file=/vagrant/.vagrant/machines/docker/virtualbox/private_key" | sudo tee -a /etc/ansible/hosts
 ansible-playbook /home/vagrant/playbooks/install-docker.yaml
